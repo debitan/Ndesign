01913c02de7dbcabe4dfd2550bbea1b6680425f6
David Matthews <dwm503@gmail.com>
2019-10-17 08:21:14 +0100

    add basic ProductPage template

---
+++ src/templates/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../components/App'
+
+const ProductPage = (product) => (
+    <App>
+        <h1>{product.title}</h1>
+    </App>
+)
+
+export default ProductPage
+
+// carry on from here https://www.gatsbyjs.org/blog/2019-05-02-how-to-build-a-blog-with-wordpress-and-gatsby-part-3/#adding-pages-to-gatsby-nodejs
