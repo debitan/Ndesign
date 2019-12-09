d8a467293db627c4afd34c3df7fc87df4ace089a
David Matthews <dwm503@gmail.com>
2019-12-09 12:50:38 +0000

    add basic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../components/App'
+
+function errorPage() {
+    return (
+        <App>
+            404
+        </App>
+    )
+}
+
+export default errorPage
